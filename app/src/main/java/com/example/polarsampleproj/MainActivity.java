25c29b180fc9c85c14f8d44282294ac00c1f0378
RicSchultheis <richard.schultheis.rs@gmail.com>
2019-12-09 12:26:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/polarsampleproj/MainActivity.java
@@ -0,0 +1,207 @@
+package com.example.polarsampleproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Bundle;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.UUID;
+
+import io.reactivex.disposables.Disposable;
+import polar.com.sdk.api.PolarBleApi;
+import polar.com.sdk.api.PolarBleApiCallback;
+import polar.com.sdk.api.PolarBleApiDefaultImpl;
+import polar.com.sdk.api.model.PolarDeviceInfo;
+import polar.com.sdk.api.model.PolarHrData;
+import polar.com.sdk.api.errors.PolarInvalidArgument;
+public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView textViewHR, textViewFW;
+    private String TAG = "Polar_HRActivity";
+    public PolarBleApi api;
+    private Disposable ecgDisposable = null;
+    private Context classContext = this;
+    private String DEVICE_ID="20BEEF24";
+    private String sharedPrefsKey = "polar_device_id";
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewHR=findViewById(R.id.dispHR);
+        sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        checkBT();
+
+        Button button =findViewById(R.id.button);
+
+
+        api = PolarBleApiDefaultImpl.defaultImplementation(this,
+                PolarBleApi.FEATURE_BATTERY_INFO |
+                        PolarBleApi.FEATURE_DEVICE_INFO |
+                        PolarBleApi.FEATURE_HR);
+        api.setApiCallback(new PolarBleApiCallback() {
+            @Override
+            public void blePowerStateChanged(boolean b) {
+                Log.d(TAG, "BluetoothStateChanged " + b);
+            }
+
+            @Override
+            public void deviceConnected(PolarDeviceInfo s) {
+                Log.d(TAG, "Device connected " + s.deviceId);
+            }
+
+            @Override
+            public void deviceConnecting(PolarDeviceInfo polarDeviceInfo) {
+
+            }
+
+            @Override
+            public void deviceDisconnected(PolarDeviceInfo s) {
+                Log.d(TAG, "Device disconnected " + s);
+
+            }
+
+            @Override
+            public void ecgFeatureReady(String s) {
+                Log.d(TAG, "ECG Feature ready " + s);
+            }
+
+            @Override
+            public void accelerometerFeatureReady(String s) {
+                Log.d(TAG, "ACC Feature ready " + s);
+            }
+
+            @Override
+            public void ppgFeatureReady(String s) {
+                Log.d(TAG, "PPG Feature ready " + s);
+            }
+
+            @Override
+            public void ppiFeatureReady(String s) {
+                Log.d(TAG, "PPI Feature ready " + s);
+            }
+
+            @Override
+            public void biozFeatureReady(String s) {
+
+            }
+
+            @Override
+            public void hrFeatureReady(String s) {
+                Log.d(TAG, "HR Feature ready " + s);
+            }
+
+            @Override
+            public void disInformationReceived(String s, UUID u, String s1) {
+                if( u.equals(UUID.fromString("00002a28-0000-1000-8000-00805f9b34fb"))) {
+                    String msg = "Firmware: " + s1.trim();
+                    Log.d(TAG, "Firmware: " + s + " " + s1.trim());
+                    textViewFW.append(msg + "\n");
+                }
+            }
+
+            @Override
+            public void batteryLevelReceived(String s, int i) {
+                String msg = "ID: " + s + "\nBattery level: " + i;
+                Log.d(TAG, "Battery level " + s + " " + i);
+//                Toast.makeText(classContext, msg, Toast.LENGTH_LONG).show();
+                textViewFW.append(msg + "\n");
+            }
+
+            @Override
+            public void hrNotificationReceived(String s,
+                                               PolarHrData polarHrData) {
+                Log.d(TAG, "HR " + polarHrData.hr);
+                List<Integer> rrsMs = polarHrData.rrsMs;
+                String msg = String.valueOf(polarHrData.hr) + "\n";
+                for (int i : rrsMs) {
+                    msg += i + ",";
+                }
+                if (msg.endsWith(",")) {
+                    msg = msg.substring(0, msg.length() - 1);
+                }
+                textViewHR.setText((polarHrData.hr));
+            }
+
+            @Override
+            public void polarFtpFeatureReady(String s) {
+                Log.d(TAG, "Polar FTP ready " + s);
+            }
+        });
+        try {
+            api.connectToDevice(DEVICE_ID);
+        } catch (PolarInvalidArgument a){
+            a.printStackTrace();
+        }
+
+        button.setOnClickListener(new OnClickListener()){
+
+        }
+    }
+
+    public void blePowerStateChanged(boolean b) {
+        Log.d(TAG, "BluetoothStateChanged " + b);
+    }
+
+    public void onClickConnect2(View view) {
+        checkBT();
+        DEVICE_ID = sharedPreferences.getString(sharedPrefsKey,"");
+        Log.d(TAG,DEVICE_ID);
+            Toast.makeText(this, DEVICE_ID,Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public void checkBT(){
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter != null && !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 2);
+        }
+
+        //requestPermissions() method needs to be called when the build SDK version is 23 or above
+        if(Build.VERSION.SDK_INT >= 23){
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        api.shutDown();
+    }
+
+    public void update() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+}
